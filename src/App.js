df03fa5618e431d0c1ea545cbbf01b4685a2f575
bramblestars <txxcui@gmail.com>
2023-10-01 02:07:10 -0500

    implement basic routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@
 import Header from './components/shared/Header';
 import RentalHome from './pages/RentalHome';
 
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { Router, Route } from './components/Router'
+
 function App() {
-  return (
-    <div>
-        <Header />
-        <RentalHome />
-    </div>
-  );
+
+    const renderPages = () => {
+        const { pathname } = window.location;
+
+        switch (pathname) {
+            case '/':
+                return <RentalHome />;
+            case '/login':
+                return <Login />;
+            case '/register':
+                return <Register />;
+            default:
+                return <RentalHome />;
+        }
+    }
+
+    return (
+        <div>
+            <Router>
+                <Header />
+                { renderPages() }
+            </Router>
+        </div>
+    );
 }
 
 export default App;
